bfb0e7a1f4f8ae267ac9203a90d50d07abe05afc
parkmawani <xkclxk@ync.ac.kr>
2023-03-09 16:19:09 +0900

    반 엔티티 추가

---
+++ src/main/java/com/project/lms/entity/ClassInfoEntity.java
@@ -0,0 +1,24 @@
+package com.project.lms.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "class_info")
+public class ClassInfoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ci_seq")
+    private Long ciSeq;
+
+    @Column(name = "ci_name", nullable = false, length = 20)
+    private String ciName;
+
+    @Column(name = "ci_limit", nullable = false)
+    private Integer ciLimit;
+
+    @Column(name = "ci_mi_seq", nullable = false)
+    private Long ciMiSeq;
+
+    // 생성자, getter, setter 등 생략
+}
