264ebf5096a0d79d87b7ebfc38fe7509daa81a7b
yuje002 <yuje0002@gmail.com>
2023-03-09 17:48:22 +0900

    entity modify

--- src/main/java/com/project/lms/config/entity/GradeInfoEntity.java
+++ src/main/java/com/project/lms/config/entity/GradeInfoEntity.java
@@ -1,6 +1,19 @@
 package com.project.lms.config.entity;
 
-import jakarta.persistence.*;
+import com.project.lms.entity.SubjectInfoEntity;
+import com.project.lms.entity.TestInfoEntity;
+import com.project.lms.entity.member.StudentInfo;
+import com.project.lms.entity.member.TeacherInfo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,16 +25,16 @@ public class GradeInfoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "gi_seq")                private Long seq;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @Column(name = "gi_sub_seq")            private Long sub_seq;
+    @JoinColumn(name = "gi_sub_seq")            private SubjectInfoEntity subject;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "gi_mi_seq1", nullable = false)
-    @Column(name = "gi_mi_seq1")            private Long mi_seq1;
+    @JoinColumn(name = "gi_mi_seq1", nullable = false)    private StudentInfo student;
+    
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "gi_mi_seq2", nullable = false)
-    @Column(name = "gi_mi_seq2")            private Long mi_seq2;
+    @JoinColumn(name = "gi_mi_seq2", nullable = false) private TeacherInfo teacher;
     @Column(name = "gi_grade")              private Integer grade;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "gi_ci_seq", nullable = false)
-    @Column(name = "gi_ci_seq")             private Long ci_seq;
+    @JoinColumn(name = "gi_test_seq", nullable = false) private TestInfoEntity test;
 }
