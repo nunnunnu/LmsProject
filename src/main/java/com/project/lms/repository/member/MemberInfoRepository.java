e4907b767496fdbe362cb4456a247028b5d38309
eun <eu-nee@naver.com>
2023-03-13 18:04:08 +0900

    20230313eun

--- src/main/java/com/project/lms/repository/member/MemberInfoRepository.java
+++ src/main/java/com/project/lms/repository/member/MemberInfoRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.lms.entity.member.MemberInfoEntity;
 
 public interface MemberInfoRepository extends JpaRepository<MemberInfoEntity, Long>{
-
     MemberInfoEntity findByMiId(String username);
-    
+    MemberInfoEntity findByMiSeq(Long miSeq);
 }
---
+++ src/main/java/com/project/lms/vo/member/UpdateMemberVO.java
@@ -0,0 +1,14 @@
+package com.project.lms.vo.member;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class UpdateMemberVO {
+    // @Schema(description = "아이디", example = "user")
+    // private String miId;
+    @Schema(description = "비밀번호", example = "123456789")
+    private String miPwd;
+    @Schema(description = "변경된 비밀번호", example = "121212121")
+    private String chngeMiPwd;
+}
