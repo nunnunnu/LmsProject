fd003d740e3e46711e9cb40381c7228095af84c4
jinhee <bis04199@gmail.com>
2023-03-15 14:27:37 +0900

    swagger Authorization buttom

--- src/main/java/com/project/lms/entity/member/enumfile/Department.java
+++ src/main/java/com/project/lms/entity/member/enumfile/Department.java
@@ -6,7 +6,7 @@ public enum Department {
     교무행정,능력개발,취업지원,상담;
 
     @JsonCreator
-    public static Department from(String name) {
+    public static Department from(String name) { //name과 일치하는 role이 없으면 null이 반환됨
         try {
             return Department.valueOf(name);
         } catch (Exception e) {
--- src/main/java/com/project/lms/entity/member/enumfile/Position.java
+++ src/main/java/com/project/lms/entity/member/enumfile/Position.java
@@ -6,7 +6,7 @@ public enum Position {
     사원,주임,실장,대리,팀장,과장,부장,원장;
 
     @JsonCreator
-    public static Position from(String name) {
+    public static Position from(String name) { //name과 일치하는 role이 없으면 null이 반환됨
         try {
             return Position.valueOf(name);
         } catch (Exception e) {
