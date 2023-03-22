503360ec31c03b4ab73e96e56ab96a9f79dca4e3
Aprilfoolsdayboy <rudwns0401@gmail.com>
2023-03-22 16:08:47 +0900

    댓글

--- src/main/java/com/project/lms/service/FeedBackService.java
+++ src/main/java/com/project/lms/service/FeedBackService.java
@@ -89,6 +89,15 @@ public class FeedBackService {
     // 피드백 상세 조회
     public ShowFeedBackDetailVO showFeedBackDetail(Long fiSeq) {
         FeedbackInfo entity = fRepo.findById(fiSeq).orElse(null);
+        List<CommentInfoEntity> comment = ciRepo.findByFeedback(entity);
+
+        List<CommentInsertVO> clist = new ArrayList<>();
+   
+        for(CommentInfoEntity c : comment) {
+        CommentInsertVO com = CommentInsertVO.builder().name(c.getMember().getMiName()).comment(c.getCmtTitle()).build();
+        clist.add(com);
+        }
+
         if(entity==null){
             ShowFeedBackDetailVO fVo = ShowFeedBackDetailVO.builder()
             .status(false)
@@ -97,8 +106,10 @@ public class FeedBackService {
             .build();
             return fVo;
         }
+
             String regDate = entity.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-            FeedBackDetailVO vo = FeedBackDetailVO.builder().title(entity.getFiTitle()).regDt(regDate).content(entity.getFiContent()).writer(entity.getTeacher().getMiName()).build();
+            FeedBackDetailVO vo = FeedBackDetailVO.builder().comment(clist).title(entity.getFiTitle()).regDt(regDate).content(entity.getFiContent())
+           .writer(entity.getTeacher().getMiName()).build();
             ShowFeedBackDetailVO fvo = ShowFeedBackDetailVO.builder()
             .status(true)
             .message("피드백 상세 조회를 하였습니다.")
