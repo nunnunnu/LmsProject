7b9a05708e281b4e332c68ab0ea367614467bb85
jinhee <bis04199@gmail.com>
2023-03-27 15:02:33 +0900

    batch insert

--- src/main/java/com/project/lms/api/ScoreAvgTotalControllaer.java
+++ src/main/java/com/project/lms/api/ScoreAvgTotalControllaer.java
@@ -14,6 +14,7 @@ import com.project.lms.vo.grade.ScoreTop10Response;
 import com.project.lms.vo.response.AvgListBuSubjectResponseVO;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -24,7 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class ScoreAvgTotalControllaer {
     private final ScoreAvgTotalService sTotalService;
     //과목별 전체 평균 조회
-    @Operation(summary = "과목별 전체 평균 조회", description = "가장 최신 테스트 순으로 과목별 평균을 조회합니다.")
+    @Operation(summary = "과목별 전체 평균 조회", description = "가장 최신 테스트 순으로 과목별 평균을 조회합니다.", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/avg")
     @Secured("ROLE_TEACHER")
     public AvgListBuSubjectResponseVO getAvgList(@AuthenticationPrincipal UserDetails userDetails){
@@ -32,7 +33,7 @@ public class ScoreAvgTotalControllaer {
         return result;
     }
     //과목별 상위 10% 평균 조회
-    @Operation(summary = "과목별 상위 10% 평균 조회", description = "가장 최신 테스트 순으로 과목별 상위 10%학생들의 평균을 조회합니다.")
+    @Operation(summary = "과목별 상위 10% 평균 조회", description = "가장 최신 테스트 순으로 과목별 상위 10%학생들의 평균을 조회합니다.", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/top10")
     @Secured("ROLE_TEACHER")
     public List<ScoreTop10Response> getScoreTestTop10() {
